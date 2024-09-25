83ae5b7d42a6333de98011294f60c831c4109d4a
FarSpawn <abhijitabhi127@gmail.com>
2024-09-25 18:13:26 +0530

    added about us, views and urls

--- project1/urls.py
+++ project1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from project1 import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about-us/', views.aboutUs)
 ]
